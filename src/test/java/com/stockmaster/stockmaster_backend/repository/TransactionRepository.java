2c182ea02e1a3b34089c475d87d131d7dda98f4a
Pratik Devang <devangpratik8@gmail.com>
2025-11-22 10:46:52 +0530

    Created interface for each entity

--- src/test/java/com/stockmaster/stockmaster_backend/repository/TransactionRepository.java
+++ src/test/java/com/stockmaster/stockmaster_backend/repository/TransactionRepository.java
@@ -3,6 +3,6 @@ package com.stockmaster.stockmaster_backend.repository;
 import com.stockmaster.stockmaster_backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface TransactionRepository extends JpaRepository<Product, Long> {
 
 }
